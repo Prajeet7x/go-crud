b112b13e0b233bb4157dfe6572eb7a5c118f5f13
root <root@LAPTOP-NRAOGJSN>
2023-03-24 20:07:36 +0545

    Feat: Able to create posts through postman

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -1,9 +1,26 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"crud/crud-module/initializers"
+	model "crud/crud-module/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PostsCreate(c *gin.Context) {
+
+	//Create a post
+	post := model.Post{Title: "Hey", Body: "First post"}
+
+	result := initializers.DB.Create(&post) // pass pointer of data to Create
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// Return it
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"post": post,
 	})
 }
